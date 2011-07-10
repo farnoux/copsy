35f254eb37b530f8cc6328833623c91f9386dfcd
Fred <arnoux.frederic@gmail.com>
2011-07-11 00:59:26 +0200

    Big rewrite of Validator objects

--- src/jquery.copsy.js
+++ src/jquery.copsy.js
@@ -5,88 +5,106 @@
 
 (function($) {
   
-  function Validator(message, validate) {
-    this.message = message;
-    this.validate = validate || function(field, value){return true;};
+  function extractKeys(object) {
+    var keys = [];
+    for(key in object) {
+      keys.push(key);
+    }
+    return keys;
   }
-    
-  Validator.prototype = {
-//    trigger: 'focus blur change keyup',
-  };
-  
-  function Field(input, validators){
-    this.input = input;
+
+  function Copsy(element, validators) {
+    this.$element = $(element);
     this.validators = validators;
   }
-  
-  Field.prototype = {
+
+  Copsy.prototype = {
+    
     // return an error message if validation fails, false otherwise.
     validate: function() {
-      var validator, valid = true, value = this.input.val();
-      $.each(this.validators, $.proxy(function(i, v){
-        validator = v;
-        // break if non valid (false value), otherwise continue (non-false value)
-        return valid = v.validate(this.input, value);
-      }, this));
-      return valid ? false : validator.message;
+      if ($.fn.copsy.skip(this.$element)) {
+        return false;
+      }
+      var v, args, value = this.$element.val();
+      for each(v in this.validators) {
+        args = (v.test.length == 2) ? [this.$element, value] : [value];
+        if (!v.test.apply(null, args)) return v.message;
+      }
+      return false;
     }  
   };
   
-  
   // Copsy jQuery plugin
-  ////////////////////////////////////////////////////////////////////////////////
-  $.fn.copsy = function(options){
-    options = $.extend({}, $.fn.copsy.defaults, options);
-    var $form = this, $fields = $form.find(':input.required, :input.email');
+  $.fn.copsy = function(options) {
+    var $form = this, 
+        $elements = $form.find(":input." + extractKeys($.fn.copsy.validators).join(",:input."));
+        // $form.find(':input.required, :input.email, :input.numeric');
+
+    // options = $.extend({}, $.fn.copsy.defaults, g);
     
-    function get_validators(ele){
-      var v = [];
+    function getValidators(ele){
+      var validators = [];
       // extract class attributes
-      $.each(ele.attr('class').split(/\s+/), function(){
-        (this in options.validators) && v.push(options.validators[this]);
-      });
-      return v;
-    }
-    
-    // Get the Field corresponding to the element
-    function get(ele){
-      var f = $.data(ele, 'field-validator');
-      if (!f) {
-        f = new Field(ele, get_validators(ele));
-        $.data(ele, 'field-validator', f);
+      for each (var name in ele.attr('class').split(/\s+/)) {
+        var v = getValidator(ele, name);
+        if (v) validators.push(v);
       }
-      return f;
+      return validators;
     }
     
-    function toggle_tooltip(ele, message){
+    function getValidator(element, name) {
+      // Is there selector-specific validators matching this name ?
+      if (name in $.fn.copsy.specifics) {
+        var specifics = $.fn.copsy.specifics[name];
+        for(var selector in specifics) {
+          if (element.is(selector)) return specifics[selector];
+        }
+      }
+      // Otherwise inspect default validators
+      if (name in $.fn.copsy.validators) {
+        return $.fn.copsy.validators[name];
+      }
+      return undefined;
+    }
+    
+    // Get the Copsy object corresponding to the element
+    function get(ele){
+      var c = $.data(ele, 'copsy');
+      if (!c) {
+        c = new Copsy(ele, getValidators(ele));
+        $.data(ele, 'copsy', c);
+      }
+      return c;
+    }
+    
+    function toggleTooltip(ele, message) {
       var tipsy = ele.tipsy(true);
-      tipsy.options.title = function(){return message;};
+      tipsy.options.title = function(){ return message; };
       message? tipsy.show() : tipsy.hide();
     }
     
-    function toggle_class(ele, valid){
+    function toggleClass(ele, valid) {
       ele[(valid?'add':'remove')+'Class']('valid');
     }
     
-    function validate(){
-      var $input = $(this);
-      if($input.is(':hidden')) return true;
+    function validate() {
+      var $element = $(this);
       
       // validate and get message if not valid
-      var message = get($input).validate();
+      var message = get($element).validate();
       
       // toggle tooltip
-      toggle_tooltip($input, message);
+      toggleTooltip($element, message);
       
       // toggle input class
-      toggle_class($input, !message);
+      toggleClass($element, !message);
       
       return !message;
     }
     
     function submit(){
       var valid = true;
-      $fields.each(function(){
+      $elements.each(function() {
         valid = validate.apply(this) && valid;
       });
       return valid;
@@ -94,10 +112,10 @@
     
     // Bind fields' trigger events
 //    $fields.tooltip({trigger: 'manual'}).bind('focus blur change keyup', validate);
-    $fields.tipsy({trigger: 'manual', gravity: 'w'});
+    $elements.tipsy({trigger: 'manual', gravity: 'w'});
     
-    $fields.filter(':radio').bind('change', validate);
-    $fields.not(':radio').bind('focus blur change keyup', validate);
+    $elements.filter(':radio').bind('change', validate);
+    $elements.not(':radio').bind('focus blur change keyup', validate);
     
     // Bind form submit
     $form.submit(submit);
@@ -106,24 +124,46 @@
   };
   
   $.fn.copsy.defaults = {
-    
   };
   
-  $.fn.copsy.defaults.validators = {
-    
-    required: new Validator("Please, specify", function(field, value){
-      if(field.is('input:radio')) return $('input[name='+ field.attr('name') +']:radio').is(':checked');
-//    if(field.is('input:radio')) return field.is(':checked');
-      return value.length > 0;
-    }),
-    
-    email: new Validator("Please, enter a valid email", function(field, v){
-      return v.length < 1 || /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(v);
-    }),
-    
-    numeric: new Validator("Please, enter only numeric values (0-9)", function(field, v){
-      return v && /^\d+$/i.test(v);
-    })
+  $.fn.copsy.skip = function(element) {
+    if (element.is(':hidden')) return true;
+  };
+  
+  
+  // 
+  //  { <className> : 
+  //    message: "validation message",
+  //    test: function([element], value) {
+  //      return ...;
+  //    }
+  //  }
+  $.fn.copsy.validators = {
+    required: {
+      message: "Please, specify", test: function(value) {
+        return value && value.length > 0;
+      }
+    },
+    email: {
+      message: "Please, enter a valid email", test: function(value) {
+        return value.length < 1 || /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(value);
+      }
+    },
+    numeric: {
+      message: "Please, enter only numeric values (0-9)", test: function(value) {
+        return value.length < 1 || /^\d+$/i.test(value);
+      }
+    }
+  };
+  
+  $.fn.copsy.specifics = {
+    required: { 
+      'input:radio': {
+        test: function(element, value) {
+          return $('input[name='+ element.attr('name') +']:radio').is(':checked');
+        }
+      }
+    }
   };
           
 })(jQuery);
