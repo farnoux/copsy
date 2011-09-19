63dad54ed16c4133072cd96b160aa85314c32e4b
Fred <arnoux.frederic@gmail.com>
2011-09-19 22:36:55 +0200

    Move tipsy adapter to its own easy-plug file

--- src/jquery.copsy.js
+++ src/jquery.copsy.js
@@ -155,33 +155,6 @@
 //
 (function ($) {
   //
-  // Add validation handlers to enable Tipsy tooltips.
-  // `validation` represent a validation Promise object.
-  //
-  $.fn.copsy.addHandler(function (validation) {
-    // Hide tooltip when validation pass
-    validation.done(function (element) {
-      var t = element.tipsy(true);
-      if (t) {
-        t.hide();
-      }
-    })
-    // Show tooltip when validation fail
-    .fail(function (element, id) {
-      var t = element.tipsy(true);
-      if (!t) {
-        element.tipsy({trigger: 'manual', gravity: 'w'});
-        t = element.tipsy(true);
-      }
-      t.options.title = function () {
-        return $.fn.copsy.message[id];
-      };
-      t.show();
-    });
-  });
-
-
-  //
   //### Define validators
   //
   $.fn.copsy.addValidator("mandatory", "input.mandatory", function (value) {
